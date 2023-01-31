ce981e3e9cd82387e4ae619acfd65de2fda22c66
Damien Vedani <damien.vedani@oca.eu>
2023-01-31 16:06:49 +0100

    Update for local API

--- src/Rap20.js
+++ src/Rap20.js
@@ -11,51 +11,31 @@ function Rap20() {
   const [artist, setArtist] = useState()
   const [image, setImage] = useState("https://cdn.music.smcloud.net/t/cover/b17cfca3-ce38-466b-bb04-570f6e8f8f0f_gf-WFey-K5xB-QSFE_eska-rap_500x500.jpg")
 
-  async function getCurrentSong() {
-    const response = await axios.get("/api/mobile/station/6190/now_playing", { headers: { "Access-Control-Allow-Origin": "*", 'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS' } })
+  async function getCurrentSong(){
+    const response = await axios.get("http://localhost:8082/test?radio=6190")
     if (response.status === 200) {
-      var endTime = new Date(response.data[3].end_time)
-      var now = new Date(Date.now())
-      var artists = ""
-      if (endTime <= now) {
-        var thumb = response.data[2].thumb
-        var names = response.data[2].name
-        setName(response.data[2].name)
-        response.data[2].artists.map((item, index) => {
+      console.log(response)
+      var thumb = response.data.thumb
+      var names = response.data.name
+        setName(response.data.name)
+        var artists
+        response.data.artists.map((item, index) => {
           artists += item.name + ", "
           return artists
         })
         artists = artists.slice(0, -2);
+        artists = artists.replace("undefined","");
         setArtist(artists)
-        if(response.data[3].image !== null){
-          setImage(response.data[2].image)
+        if(response.data.image !== null){
+          setImage(response.data.image)
         }else{
           setImage("https://cdn.music.smcloud.net/t/cover/b17cfca3-ce38-466b-bb04-570f6e8f8f0f_gf-WFey-K5xB-QSFE_eska-rap_500x500.jpg")
         }
         updateMetaData(names,artists, image,thumb)
       } else {
-        thumb = response.data[3].thumb
-        names = response.data[3].name
-        setName(response.data[3].name)
-        response.data[3].artists.map((item, index) => {
-          artists += item.name + ", "
-          return artists
-        })
-        artists = artists.slice(0, -2);
-        setArtist(artists)
-        if(response.data[3].image !== null){
-          setImage(response.data[3].image)
-        }else{
-          setImage("https://cdn.music.smcloud.net/t/cover/b17cfca3-ce38-466b-bb04-570f6e8f8f0f_gf-WFey-K5xB-QSFE_eska-rap_500x500.jpg")
-        }
-        
-        updateMetaData(names,artists, image,thumb)
+        console.error("Something went wrong");
       }
-      
-    } else {
-      console.error("Something went wrong");
-    }
-    setTimeout(getCurrentSong, 10 * 1000)
+      setTimeout(getCurrentSong, 10 * 1000)
   }
 
   function updateMetaData(nazywa,artyst,image,thumb) {

59e25bcdf5d63a39362edaf19ca8a39101815768
Damien Vedani <damien.vedani@oca.eu>
2023-02-13 15:59:05 +0100

    update previous and next song

--- src/App.js
+++ src/App.js
@@ -11,41 +11,85 @@ import axios from 'axios';
 function App() {
   const [radio, setRadio] = useState("")
   useEffect(() => {
+    if(radio !== ""){
     getCurrentSong()
+  }
     // eslint-disable-next-line 
   }, [radio])
 
   const [name, setName] = useState("")
   const [artist, setArtist] = useState()
   const [image, setImage] = useState("")
+  const [previousName, setPreviousName] = useState("")
+  const [previousArtist, setPreviousArtist] = useState()
+  const [previousImage, setPreviousImage] = useState("")
+  const [nextName, setNextName] = useState("")
+  const [nextArtist, setNextArtist] = useState()
+  const [nextImage, setNextImage] = useState("")
   const [time, setTime] = useState()
-
-  // const [changed, setChanged] = useState(false)
 
   const defineRadio = (result) => {
     setRadio(result)
-    // setChanged(true)
   };
 
   async function getCurrentSong() {
     clearTimeout(time)
     console.log(radio)
-    const response = await axios.get(`https://damien.lensalex.fr:3000/eska?radio=${radio}`)
+    // const response = await axios.get(`https://damien.lensalex.fr:3000/eska?radio=${radio}`)
+    const response = await axios.get(`http://192.168.109.184:8082/test?radio=${radio}`)
     if (response.status === 200) {
-      console.log(response)
-      var thumb = response.data.thumb
-      var names = response.data.name
-      setName(response.data.name)
+      console.log(response.data[1])
+      var thumb = response.data[1].thumb
+      var names = response.data[1].name
+      // Handle names of the songs
+      setNextName(response.data[0].name)
+      setName(response.data[1].name)
+      setPreviousName(response.data[2].name)
+      // Handle artists
+      var nextArtists
+      response.data[0].artists.map((item, index) => {
+        nextArtists += item.name + ", "
+        return nextArtists
+      })
+      nextArtists = nextArtists.slice(0, -2);
+      nextArtists = nextArtists.replace("undefined", "");
+      setNextArtist(nextArtists)
       var artists
-      response.data.artists.map((item, index) => {
+      response.data[1].artists.map((item, index) => {
         artists += item.name + ", "
         return artists
       })
       artists = artists.slice(0, -2);
       artists = artists.replace("undefined", "");
       setArtist(artists)
-      if (response.data.image !== null) {
-        setImage(response.data.image)
+      var previousArtists
+      response.data[2].artists.map((item, index) => {
+        previousArtists += item.name + ", "
+        return previousArtists
+      })
+      previousArtists = previousArtists.slice(0, -2);
+      previousArtists = previousArtists.replace("undefined", "");
+      setPreviousArtist(previousArtists)
+      // Handle image
+      if (response.data[0].image !== null) {
+        setNextImage(response.data[0].image)
+      } else {
+        switch(radio){
+          case "6130":
+            setNextImage("https://cdn.music.smcloud.net/t/cover/5c456bee-6451-4298-bdca-9d7e31f96e91_gf-SH8U-7BAA-AeWq_eska-2xg20_500x500.jpg")
+            break;
+          case "6030":
+            setNextImage("https://cdn.music.smcloud.net/t/cover/2a264178-a393-4781-ba01-ff7c3894df8a_gf-mBV3-pxB4-b8g5_eska-imprezka_500x500.jpg")
+            break;
+          case "6190":
+            setNextImage("https://cdn.music.smcloud.net/t/cover/b17cfca3-ce38-466b-bb04-570f6e8f8f0f_gf-WFey-K5xB-QSFE_eska-rap_500x500.jpg")
+            break;
+          default:
+            setNextImage("https://cdn.music.smcloud.net/t/cover/602854b3-a955-4737-9588-ea963676f73c_ESKA_radio_500x500_500x500.jpg")
+        }
+      }
+      if (response.data[1].image !== null) {
+        setImage(response.data[1].image)
       } else {
         switch(radio){
           case "6130":
@@ -59,6 +103,23 @@ function App() {
             break;
           default:
             setImage("https://cdn.music.smcloud.net/t/cover/602854b3-a955-4737-9588-ea963676f73c_ESKA_radio_500x500_500x500.jpg")
+        }
+      }
+      if (response.data[2].image !== null) {
+        setPreviousImage(response.data[2].image)
+      } else {
+        switch(radio){
+          case "6130":
+            setPreviousImage("https://cdn.music.smcloud.net/t/cover/5c456bee-6451-4298-bdca-9d7e31f96e91_gf-SH8U-7BAA-AeWq_eska-2xg20_500x500.jpg")
+            break;
+          case "6030":
+            setPreviousImage("https://cdn.music.smcloud.net/t/cover/2a264178-a393-4781-ba01-ff7c3894df8a_gf-mBV3-pxB4-b8g5_eska-imprezka_500x500.jpg")
+            break;
+          case "6190":
+            setPreviousImage("https://cdn.music.smcloud.net/t/cover/b17cfca3-ce38-466b-bb04-570f6e8f8f0f_gf-WFey-K5xB-QSFE_eska-rap_500x500.jpg")
+            break;
+          default:
+            setPreviousImage("https://cdn.music.smcloud.net/t/cover/602854b3-a955-4737-9588-ea963676f73c_ESKA_radio_500x500_500x500.jpg")
         }
       }
       updateMetaData(names, artists, image, thumb)
@@ -99,7 +160,7 @@ function App() {
         <Routes>
           <Route path='/' exact element={<Dom link={link} city={city} defineRadio={defineRadio} name={name} artist={artist} image={image} />} />
           <Route path='/goraca20' exact element={<Goraca20 city={city} defineRadio={defineRadio} name={name} artist={artist} image={image} />} />
-          <Route path='/rap20' exact element={<Rap20 city={city} defineRadio={defineRadio} name={name} artist={artist} image={image} />} />
+          <Route path='/rap20' exact element={<Rap20 city={city} defineRadio={defineRadio} name={name} artist={artist} image={image} nextArtist={nextArtist} nextImage={nextImage} nextName={nextName} previousArtist={previousArtist} previousImage={previousImage} previousName={previousName}/>} />
           <Route path='/eskaImpreska' exact element={<Impreska city={city} defineRadio={defineRadio} name={name} artist={artist} image={image} />} />
         </Routes>
       </Router>
--- src/Rap20.js
+++ src/Rap20.js
@@ -1,20 +1,49 @@
 import React, { useEffect } from 'react'
 import PlayerRap from './PlayerRap'
 
-import { Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import { Box, Stack } from '@mui/system';
 
-function Rap20({defineRadio,name,artist,image}) {
+function Rap20({ defineRadio, name, artist, image, previousName, previousArtist, previousImage, nextArtist, nextImage, nextName }) {
   useEffect(() => {
     radio()
-  }, )
-  const radio = async() => {
+  },)
+  const radio = async () => {
     defineRadio("6190")
   }
   document.title = "Rap 20 - ESKA";
   return (
     <>
-      <Typography sx={{color: "#eb7d2b", py:2}}>Słuchaj Rap 20</Typography>
+      <Typography sx={{ color: "#eb7d2b", py: 2 }}>Słuchaj Rap 20</Typography>
       <PlayerRap name={name} artist={artist} image={image} />
+        <div >
+          <Grid container spacing={2}>
+            <Grid sx={{minWidth:"200px"}} item xs={6} md={6}>
+              <Typography>Poprzednia</Typography>
+              <Stack direction="row" spacing={2}>
+                <div>
+                  <Box component="img" sx={{ height: "75px", width: "75px", borderRadius: "5px" }} alt="Eska RAP 20" src={previousImage} />
+                </div>
+                <div>
+                  <Typography id="title_bis">{previousName}</Typography>
+                  <Typography id="artist_bis">{previousArtist}</Typography>
+                </div>
+              </Stack>
+            </Grid>
+            <Grid sx={{minWidth:"200px"}} item xs={6} md={6}>
+              <Typography>Następny</Typography>
+              <Stack direction="row" spacing={2}>
+                <div>
+                  <Box component="img" sx={{ height: "75px", width: "75px", borderRadius: "5px" }} alt="Eska RAP 20" src={nextImage} />
+                </div>
+                <div>
+                  <Typography id="title_bis">{nextName}</Typography>
+                  <Typography id="artist_bis">{nextArtist}</Typography>
+                </div>
+              </Stack>
+            </Grid>
+          </Grid>
+        </div>
     </>
   )
 }

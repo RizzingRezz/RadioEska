adf7151b6ce52039d1107b2c9e7bc7512545b0cd
Damien Vedani <damien.vedani@oca.eu>
2023-02-14 15:52:53 +0100

    change player

--- src/Player.js
+++ src/Player.js
@@ -1,48 +1,71 @@
 import React, { useState } from 'react';
-import ReactHlsPlayer from "@ducanh2912/react-hls-player";
+// import ReactHlsPlayer from "@ducanh2912/react-hls-player";
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 import PauseCircleFilledIcon from '@mui/icons-material/PauseCircleFilled';
 import { Box } from '@mui/system';
 import { Typography } from '@mui/material';
+import ReactPlayer from 'react-player';
 
 function Player({link,name,artist,image}) {
-    const [isPlayed, setIsPlayed] = useState(true)
-    const handlePlayed = () => {
-        if (isPlayed) {
-            setIsPlayed(false);
-        }
-        if (!isPlayed) {
-            setIsPlayed(true)
-        }
-    }
-    const playerRef = React.useRef();
+    // const [isPlayed, setIsPlayed] = useState(true)
+    // const handlePlayed = () => {
+    //     if (isPlayed) {
+    //         setIsPlayed(false);
+    //     }
+    //     if (!isPlayed) {
+    //         setIsPlayed(true)
+    //     }
+    // }
+    // const playerRef = React.useRef();
 
-    function playVideo() {
-        playerRef.current.play();
-        handlePlayed()
+    // function playVideo() {
+    //     playerRef.current.play();
+    //     handlePlayed()
+    // }
+
+    // function pauseVideo() {
+    //     playerRef.current.pause();
+    //     handlePlayed()
+    // }
+    const [isPlayed, setIsPlayed] = useState(false)
+    const [playing, setPlaying] = useState(false)
+
+    function handlePlayClick() {
+        setIsPlayed(true)
+
+        setPlaying(true)
     }
 
-    function pauseVideo() {
-        playerRef.current.pause();
-        handlePlayed()
+    function handlePauseClick() {
+        setIsPlayed(false);
+        setPlaying(false)
     }
     return (
         <div className='player'>
             <Box component="img" sx={{height:"180px", width:"180px",borderRadius: "15px" }}  alt="Eska: Hity na czasy" src={image}/>
             <br/>
             {isPlayed ?
-                <PlayCircleFilledWhiteIcon sx={{ color: "#eb7d2b", width: '60px', height: '60px' }} onClick={playVideo} />
+                <PauseCircleFilledIcon sx={{ color: "#eb7d2b", width: '60px', height: '60px' }} onClick={handlePauseClick} />
                 :
-                <PauseCircleFilledIcon sx={{ color: "#eb7d2b", width: '60px', height: '60px' }} onClick={pauseVideo} />
+                
+                <PlayCircleFilledWhiteIcon sx={{ color: "#eb7d2b", width: '60px', height: '60px' }} onClick={handlePlayClick} />
             }
-            <ReactHlsPlayer
+            <ReactPlayer
+                    playing={playing}
+                    url={link}
+                    controls
+                    playsinline
+                    preload="metadata"
+                    style={{ display: "none" }}
+            />
+            {/* <ReactHlsPlayer
                 src={link}
                 playsInline
                 preload="metadata"
                 autoPlay={false}
                 playerRef={playerRef}
                 style={{ display: "none" }}
-            />
+            /> */}
             <Typography id="title">{name}</Typography>
             <Typography id="artist">{artist}</Typography>
         </div>
--- src/PlayerImpreska.js
+++ src/PlayerImpreska.js
@@ -1,47 +1,41 @@
 import React, { useState } from 'react';
-import ReactHlsPlayer from "@ducanh2912/react-hls-player";
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 import PauseCircleFilledIcon from '@mui/icons-material/PauseCircleFilled';
 import { Box, Typography } from '@mui/material';
+import ReactPlayer from 'react-player';
 
 function PlayerImpreska({name,artist,image}) {
-    const [isPlayed, setIsPlayed] = useState(true)
-    const handlePlayed = () => {
-        if (isPlayed) {
-            setIsPlayed(false);
-        }
-        if (!isPlayed) {
-            setIsPlayed(true)
-        }
-    }
-    const playerRef = React.useRef();
+    const [isPlayed, setIsPlayed] = useState(false)
+    const [playing, setPlaying] = useState(false)
 
-    function playVideo() {
-        playerRef.current.play();
-        handlePlayed()
+    function handlePlayClick() {
+        setIsPlayed(true)
+
+        setPlaying(true)
     }
 
-    function pauseVideo() {
-        playerRef.current.pause();
-        handlePlayed()
+    function handlePauseClick() {
+        setIsPlayed(false);
+        setPlaying(false)
     }
     return (
         <div className='player'>
             <Box component="img" sx={{height:"180px", width:"180px",borderRadius: "15px" }}  alt="Eska IMPRESKA" src={image}/>
             <br />
             {isPlayed ?
-                <PlayCircleFilledWhiteIcon sx={{ color: "#eb7d2b", width: '60px', height: '60px' }} onClick={playVideo} />
+            <PauseCircleFilledIcon sx={{ color: "#eb7d2b", width: '60px', height: '60px' }} onClick={handlePauseClick} />
+                
                 :
-                <PauseCircleFilledIcon sx={{ color: "#eb7d2b", width: '60px', height: '60px' }} onClick={pauseVideo} />
+                <PlayCircleFilledWhiteIcon sx={{ color: "#eb7d2b", width: '60px', height: '60px' }} onClick={handlePlayClick} />
             }
-            <ReactHlsPlayer
-                src="https://radio.stream.smcdn.pl/icradio-p/6030-1.aac/playlist.m3u8"
-                playsInline
-                preload="metadata"
-                autoPlay={false}
-                playerRef={playerRef}
-                style={{ display: "none" }}
-            />
+            <ReactPlayer
+                    playing={playing}
+                    url="https://radio.stream.smcdn.pl/icradio-p/6030-1.aac/playlist.m3u8"
+                    controls
+                    playsinline
+                    preload="metadata"
+                    style={{ display: "none" }}
+                />
             <Typography id="title">{name}</Typography>
             <Typography id="artist">{artist}</Typography>
         </div>
